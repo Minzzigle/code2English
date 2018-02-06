6c285125fbdccc6c825b4d39585aac7bfa63c0e8
hogle.j <hogle.j@kakaocorp.com>
2018-02-06 18:56:36 +0900

    first commit

---
+++ src/main/java/com/minzzigle/codetoenglish/CodeToEnglishApplication.java
@@ -0,0 +1,12 @@
+package com.minzzigle.codetoenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeToEnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeToEnglishApplication.class, args);
+	}
+}
---
+++ src/test/java/com/minzzigle/codetoenglish/CodeToEnglishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.minzzigle.codetoenglish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeToEnglishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
